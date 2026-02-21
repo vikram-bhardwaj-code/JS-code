6569cb5740c334521da1cbc6f2316773ab4d55b7
vikram-bhardwaj-code <vickyystudy26910@gmail.com>
2026-02-21 16:10:09 +0000

    types of datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+let name = "" // string type
+let age = 23
+let number = 1
+let isTrue = true
+let userId = null
+let address;
+
+console.table([name,age,number,isTrue,userId,address])
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// object
+//null
+//undefined
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
