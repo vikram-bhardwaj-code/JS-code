c0dd663a9118f313bfec92fae8fa1ef7e97a35a3
vikram-bhardwaj-code <vickyystudy26910@gmail.com>
2026-02-21 10:23:35 +0000

    type of varaiables

---
+++ 01_basics/varaibles.js
@@ -0,0 +1,13 @@
+let userName = "Vikram"
+const userId = 123456
+userAge = 12
+let userAddress;
+
+/*
+var userAge=12
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(userName,userAddress)
+console.table([userName,userId,userAge,userAddress])
